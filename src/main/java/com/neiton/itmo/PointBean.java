0ad6103545b4ac480f655265a431d33ef603b198
neiton <neitonmatisso@gmail.com>
2022-01-18 14:31:45 +0300

    add input X and Y, change styles, change first canvas drawing

--- src/main/java/com/neiton/itmo/PointBean.java
+++ src/main/java/com/neiton/itmo/PointBean.java
@@ -2,7 +2,26 @@ package com.neiton.itmo;
 
 
 public class PointBean {
+
+  private double X;
+  private double Y;
   private double R;
+
+  public double getX() {
+    return X;
+  }
+
+  public void setX(double x) {
+    X = x;
+  }
+
+  public double getY() {
+    return Y;
+  }
+
+  public void setY(double y) {
+    Y = y;
+  }
 
   public double getR() {
     return R;
